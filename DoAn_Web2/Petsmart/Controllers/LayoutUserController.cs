ec3380b2d3a46f25ac9a2456803712f4b24e2b27
Tuong Nguyen <tuongnguyendev@gmail.com>
2017-05-03 22:36:26 +0700

    Add database and ParitalView GetMenu

---
+++ DoAn_Web2/Petsmart/Controllers/LayoutUserController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Petsmart.Models;
+namespace Petsmart.Controllers
+{
+    public class LayoutUserController : Controller
+    {
+       ShopBanDongVatEntities db = new ShopBanDongVatEntities();
+        // GET: LayoutUser
+      public PartialViewResult getMenu()
+      {
+            var LoaiSanPham = db.LoaiSanPhams.ToList();
+            return PartialView(LoaiSanPham);
+      }
+    }
+}
