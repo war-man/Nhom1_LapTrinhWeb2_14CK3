6625a409326734550a3871e7195ee3b5acdfe855
Le Huy Vu <real.madrid29051996@gmail.com>
2017-05-04 19:22:46 +0700

    Bo sung chuc nang hien thi Danh Muc Hang Dau va San Pham Ban Chay

--- DoAn_Web2/Petsmart/Controllers/HomeController.cs
+++ DoAn_Web2/Petsmart/Controllers/HomeController.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Petsmart.Models;
 namespace Petsmart.Controllers
 {
     public class HomeController : Controller
     {
+        private ShopBanDongVatEntities db = new ShopBanDongVatEntities();
         public ActionResult Index()
-        {
+        {   
             return View();
+        }
+
+        public PartialViewResult DanhMucHangDau()
+        {
+            List<SanPham> lstSanPham = db.SanPhams.ToList();
+            return PartialView(lstSanPham);
+        }
+
+        public PartialViewResult SanPhamBanChay()
+        {
+            List<SanPham> lstSanPham = db.SanPhams.ToList();
+            return PartialView(lstSanPham);
         }
 
         public ActionResult Contact()
