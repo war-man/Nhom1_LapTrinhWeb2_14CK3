f94f90d87d081e33ff5aa0e5a29f5632ed710f42
Tuong Nguyen <tuongnguyendev@gmail.com>
2017-05-05 21:07:13 +0700

    Hoan thanh gio hang

---
+++ DoAn_Web2/Petsmart/Controllers/ItemCart.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Petsmart.Models;
+namespace Petsmart.Controllers
+{
+    public class ItemCart
+    {
+        private SanPham sp = new SanPham();
+        private int soluong;
+
+        public SanPham Sp
+        {
+            get
+            {
+                return sp;
+            }
+
+            set
+            {
+                sp = value;
+            }
+        }
+
+        public int Soluong
+        {
+            get
+            {
+                return soluong;
+            }
+
+            set
+            {
+                soluong = value;
+            }
+        }
+        public ItemCart(SanPham sp ,int soluong)
+        {
+            this.Sp = sp;
+            this.Soluong = soluong;
+        }
+    }
+}
--- DoAn_Web2/Petsmart/Controllers/ShoppingCartController.cs
+++ DoAn_Web2/Petsmart/Controllers/ShoppingCartController.cs
@@ -3,19 +3,87 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Petsmart.Models;
 namespace Petsmart.Controllers
 {
     public class ShoppingCartController : Controller
     {
+        private ShopBanDongVatEntities db = new ShopBanDongVatEntities();
         // GET: ShoppingCart
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult OrderNow(int id)
+        private int KiemTraSanPham(int id)
+        {
+            List<ItemCart> cart = (List<ItemCart>)Session["cart"];
+            for(int i = 0; i< cart.Count; i++)
+            {
+                if(cart[i].Sp.MaSanPham == id)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public ActionResult ViewCart()
         {
             return View();
+        }
+        public ActionResult OrderNow(int id)
+        {
+            // Xử lý nút thêm
+            if (Session["cart"] == null)
+            {
+                List<ItemCart> cart = new List<ItemCart>();
+                cart.Add(new ItemCart(db.SanPhams.Find(id), 1));
+                Session["cart"] = cart;
+            }
+            else
+            {
+                List<ItemCart> cart = (List<ItemCart>)Session["cart"];
+                int index = KiemTraSanPham(id);
+                if(index == -1)
+                {
+                    cart.Add(new ItemCart(db.SanPhams.Find(id), 1));
+                }
+                else
+                {
+                    cart[index].Soluong++;
+                }
+                Session["cart"] = cart;
+            }
+            return RedirectToAction("ViewCart", "ShoppingCart");
+        }
+        public ActionResult UpdateCart(FormCollection frm)
+        {
+            string[] quantities = frm.GetValues("quantity");
+            List<ItemCart> cart = (List<ItemCart>)Session["cart"];
+            for (int i = 0; i < cart.Count; i++)
+                cart[i].Soluong = Convert.ToInt32(quantities[i]);
+
+            Session["cart"] = cart;
+            return RedirectToAction("ViewCart", "ShoppingCart");
+        }
+        public ActionResult RemoveCart(int id)
+        {
+            int index = KiemTraSanPham(id);
+            List<ItemCart> cart = (List<ItemCart>)Session["cart"];
+            if (index != -1)
+            {
+                cart.RemoveAt(index);
+            }
+            Session["cart"] = cart;
+            return RedirectToAction("ViewCart", "ShoppingCart");
+        }
+        public ActionResult RemoveAll()
+        {
+            Session["cart"] = null;
+            return RedirectToAction("ViewCart", "ShoppingCart");
+        }
+        public ActionResult CheckOut()
+        {
+            return RedirectToAction("ViewCart", "ShoppingCart");
         }
 
         public ActionResult WishList()
