f623c0c6158ec8214fbdab572bf7d927a311d35b
Le Huy Vu <real.madrid29051996@gmail.com>
2017-05-05 22:18:51 +0700

    Bo sung trang them chuc nang chi tiet gio hang, cac san pham tuong tu

---
+++ DoAn_Web2/Petsmart/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Petsmart.Models;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Petsmart.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ShopBanDongVatEntities db = new ShopBanDongVatEntities();
+        // GET: Products
+        public ActionResult Detail(int id)
+        {
+            ViewData["ChitietSP"] = db.SanPhams.Where(s => s.MaSanPham == id).Select(s => s).ToList();
+            var maloai = db.SanPhams.Where(s => s.MaSanPham == id).Select(s => s.MaLoaiSanPham);
+            ViewData["SPTuongTu"] = db.SanPhams.Where(s => s.MaLoaiSanPham == maloai.FirstOrDefault() && s.MaSanPham != id).Select(s => s).ToList();
+            ViewData["LoaiSP"] = db.LoaiSanPhams.Where(s => s.MaLoaiSanPham == maloai.FirstOrDefault()).Select(s => s).ToList();
+            return View();
+        }
+    }
+}
