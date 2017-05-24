923b19d919cbdb10a0a02a80de6e0a7619c92d8c
Tuong Nguyen <tuongnguyendev@gmail.com>
2017-05-24 18:17:25 +0700

    Hoàn thành chức năng quản lý sản phẩm, Sửamenu layout admin Thêm thư mục lib_alert.. Thư viện desgin alert.

--- DoAn_Web2/Petsmart/Controllers/AProductsController.cs
+++ DoAn_Web2/Petsmart/Controllers/AProductsController.cs
@@ -82,16 +82,176 @@ namespace Petsmart.Controllers
             ViewBag.HangSanXuats = new SelectList(hsx, "MaHangSanXuat", "TenHangSanXuat");
             return View();
         }
+
+        [HttpPost]
+        public string themsanpham(FormCollection fm)
+        {
+
+            if (Request.Files.Count > 0)
+            {
+                SanPham sp = new SanPham();
+                sp.TenSanPham = Convert.ToString(fm["tensanpham"].Trim());
+                sp.SoLuongTon = Convert.ToInt32(fm["soluong"]);
+                sp.GiaSanPham = Convert.ToDecimal(fm["giaban"]);
+                sp.MaLoaiSanPham = Convert.ToInt32(fm["MaLoaiSanPham"]);
+                sp.MaHangSanXuat = Convert.ToInt32(fm["MaHangSanXuat"]);
+                sp.MoTa = Convert.ToString(fm["mota"].Trim());
+
+                // default
+                sp.LuotXem = 0;
+                sp.NgayNhap = DateTime.Now;
+                sp.BiXoa = false;
+                sp.SoLuongBan = 0;
+                try
+                {
+                    // get hình đại diện
+                    HttpPostedFileBase hinhdaidien = Request.Files["image"];
+                    //  Get all files from Request object  
+                    string fname;
+                    int idmax = 1;
+                    int idmax2 = 0;
+                    if (db.HinhAnhSanPhams.Count() > 0)
+                        idmax2 = db.HinhAnhSanPhams.OrderByDescending(u => u.MaHinh).FirstOrDefault().MaHinh;
+
+                    if (db.SanPhams.Count() > 0)
+                    {
+                        idmax = db.SanPhams.OrderByDescending(u => u.MaSanPham).FirstOrDefault().MaSanPham;
+                    }
+
+                    HttpFileCollectionBase files = Request.Files;
+
+                    for (int i = 0; i < files.Count; i++)
+                    {
+                        HttpPostedFileBase file = files[i];
+                        string fileName;// lưu rename tên file
+                        if (file.FileName == hinhdaidien.FileName)
+                        {
+                            // lưu hình đại diện
+                            idmax = idmax + 1;
+                            FileInfo fi = new FileInfo(file.FileName);
+                            string ext = fi.Extension;
+                            // ảnh đại diện
+                            fileName = "p-" + idmax.ToString() + ext;
+                            sp.HinhURL = fileName;
+                            fname = Path.Combine(Server.MapPath("~/Content/images/products/"), fileName);
+                            file.SaveAs(fname);
+                            db.SanPhams.Add(sp);
+                        }
+                        else
+                        {
+                            idmax2 = idmax2 + 1;
+                            FileInfo fi = new FileInfo(file.FileName);
+                            string ext = fi.Extension;
+                            // Upload vào table HinhAnhSanPham
+                            fileName = "slide-p-" + idmax.ToString() + "-" + idmax2.ToString() + ext;
+                            HinhAnhSanPham hssp = new HinhAnhSanPham();
+                            hssp.MaSanPham = sp.MaSanPham;
+                            hssp.HinhURL = fileName;
+                            db.HinhAnhSanPhams.Add(hssp);
+                            fname = Path.Combine(Server.MapPath("~/Content/images/products/"), fileName);
+                            file.SaveAs(fname);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return "error_images";
+                }
+            }
+            else
+            {
+                return "error_images";
+            }
+            if (db.SaveChanges() > 0)
+            {
+                return "success";
+            }
+            else
+            {
+                return "Lỗi sản phẩm";
+            }
+        }
+        [HttpPost]
+        public string suasanpham(FormCollection fm)
+        {
+            int id = Convert.ToInt32(fm["masanpham"]);
+            SanPham sp = db.SanPhams.SingleOrDefault(s => s.MaSanPham == id);
+            if (sp == null)
+            {
+                return "Không tồn tại sản phẩm này";
+            }
+            sp.TenSanPham = Convert.ToString(fm["tensanpham"].Trim());
+            sp.SoLuongTon = Convert.ToInt32(fm["soluong"]);
+            sp.GiaSanPham = Convert.ToDecimal(fm["giaban"]);
+            sp.MaLoaiSanPham = Convert.ToInt32(fm["MaLoaiSanPham"]);
+            sp.MaHangSanXuat = Convert.ToInt32(fm["MaHangSanXuat"]);
+            sp.MoTa = Convert.ToString(fm["mota"].Trim());
+
+
+            HttpPostedFileBase hinhdaidien = Request.Files["image"];
+            HttpFileCollectionBase files = Request.Files;
+            HttpPostedFileBase file_check = files[1];
+            // check có file mới để upload hay không
+            if (file_check.FileName != "" || hinhdaidien.FileName != "")
+            {
+                // Check nếu có file upload
+                string fname;
+                int idmax = 1;
+                int idmax2 = 0;
+                if (db.HinhAnhSanPhams.Count() > 0)
+                    idmax2 = db.HinhAnhSanPhams.OrderByDescending(u => u.MaHinh).FirstOrDefault().MaHinh;
+
+                if (db.SanPhams.Count() > 0)
+                {
+                    idmax = db.SanPhams.OrderByDescending(u => u.MaSanPham).FirstOrDefault().MaSanPham;
+                }
+                for (int i = 0; i < files.Count; i++)
+                {
+                    HttpPostedFileBase file = files[i];
+                    string fileName;// lưu rename tên file
+                    // nếu có thay đổi hình đại diện: xóa hình cũ và upload hinh mới(replace ảnh đại diện)
+                    if (file.FileName == hinhdaidien.FileName && file.FileName != "")
+                    {
+                        // lưu hình đại diện
+                        FileInfo fi = new FileInfo(file.FileName);
+                        string ext = fi.Extension;
+                        // ảnh đại diện
+                        fname = Path.Combine(Server.MapPath("~/Content/images/products/"), sp.HinhURL);
+                        file.SaveAs(fname);
+                    }
+                    else
+                    {
+                        if (file.FileName != "")
+                        {
+                            // upload hình slide thêm vào
+                            idmax2 = idmax2 + 1;
+                            FileInfo fi = new FileInfo(file.FileName);
+                            string ext = fi.Extension;
+                            // Upload vào table HinhAnhSanPham
+                            fileName = "slide-p-" + idmax.ToString() + "-" + idmax2.ToString() + ext;
+                            HinhAnhSanPham hssp = new HinhAnhSanPham();
+                            hssp.MaSanPham = sp.MaSanPham;
+                            hssp.HinhURL = fileName;
+                            db.HinhAnhSanPhams.Add(hssp);
+                            fname = Path.Combine(Server.MapPath("~/Content/images/products/"), fileName);
+                            file.SaveAs(fname);
+                        }
+                    }
+                }
+            }
+            db.SaveChanges();
+            return "success";
+        }
         // Ajax show popover detail product
         [HttpPost]
         public string DetailProduct(int id)
         {
             SanPham sp = db.SanPhams.Single(s => s.MaSanPham == id);
             string img = "<img src='../Content/images/products/" + sp.HinhURL + "' class='img-circle img-sm' />";
-            string out1 = img+"<div class='row'>" +
+            string out1 = img + "<div class='row'>" +
                                 "<p>" +
                                     "<label>Tên:&nbsp;</label>" +
-                                    "<strong>"+ sp.TenSanPham +"</strong>"+
+                                    "<strong>" + sp.TenSanPham + "</strong>" +
                                 "</p>" +
                                  "<p>" +
                                     "<label>Giá:&nbsp;</label>" +
@@ -100,10 +260,14 @@ namespace Petsmart.Controllers
                                  "<p>" +
                                     "<label>Thuộc loại:&nbsp;</label>" +
                                     "<strong>" + sp.LoaiSanPham.TenLoaiSanPham + "</strong>" +
-                                "</p>"+
+                                "</p>" +
                                 "<p>" +
                                     "<label>Đối tác:&nbsp;</label>" +
                                     "<strong>" + sp.HangSanXuat.TenHangSanXuat + "</strong>" +
+                                "</p>" +
+                                "<p>" +
+                                    "<label>Số lượng tồn:&nbsp;</label>" +
+                                    "<strong>" + sp.SoLuongTon + "</strong>" +
                                 "</p>" +
                                 "<p>" +
                                     "<label>Số lượng bán:&nbsp;</label>" +
@@ -116,93 +280,55 @@ namespace Petsmart.Controllers
                         "</div>";
             return out1;
         }
-        [HttpPost]
-        public string themsanpham(FormCollection fm)
+
+        public ActionResult UpdateProduct(int id)
         {
-            SanPham sp = new SanPham();
-            sp.TenSanPham = Convert.ToString(fm["tensanpham"].Trim());
-            sp.SoLuongTon = Convert.ToInt32(fm["soluong"]);
-            sp.GiaSanPham = Convert.ToDecimal(fm["giaban"]);
-            sp.MaLoaiSanPham = Convert.ToInt32(fm["MaLoaiSanPham"]);
-            sp.MaHangSanXuat = Convert.ToInt32(fm["MaHangSanXuat"]);
-            sp.MoTa = Convert.ToString(fm["mota"].Trim());
+            SanPham sp = db.SanPhams.SingleOrDefault(s => s.MaSanPham == id);
 
-            // default
-            sp.LuotXem = 0;
-            sp.NgayNhap = DateTime.Now;
-            sp.SoLuongTon = 0;
-            sp.BiXoa = false;
-
-            // Thêm hình ảnh
-            string json = Convert.ToString(fm["_images"]);
-            List<ImagesProduct> lstImages = JsonConvert.DeserializeObject<List<ImagesProduct>>(json);
-
-            // Lấy hình 1 đại diện mặc định khi user không chọn
-
-            // Get ID MAX table SanPhams
-            int idmax = 0;
-            idmax = db.SanPhams.OrderByDescending(u => u.MaSanPham).FirstOrDefault().MaSanPham;
-            idmax = idmax + 1;
-
-            //GET ID MAX table HinhANHSANPHAM
-            int idmax2 = 0;
-            idmax2 = db.HinhAnhSanPhams.OrderByDescending(u => u.MaHinh).FirstOrDefault().MaHinh;
-            var fileName ="";
-            var path="";
-
-            // kiểm tra xem đã đó lựa chọn chưa
-            bool checkselected = false;
-            foreach (var item in lstImages)
+            if (sp == null)
             {
-                if(item.Status == true)
-                {
-                    checkselected = true;
-                }
+                RedirectToAction("Index");
             }
-            if(checkselected == false)
+            List<HinhAnhSanPham> hsp = db.HinhAnhSanPhams.Where(h => h.MaSanPham == id).ToList();
+
+            ViewData["ListImagesPro"] = hsp;
+            ViewData["SanPham"] = sp;
+            List<LoaiSanPham> lsp = db.LoaiSanPhams.ToList<LoaiSanPham>();
+            var selectedValue = sp.MaLoaiSanPham;
+            ViewBag.LoaiSanPhams = new SelectList(lsp, "MaLoaiSanPham", "TenLoaiSanPham", selectedValue);
+            var selectedValue1 = sp.MaHangSanXuat;
+            List<HangSanXuat> hsx = db.HangSanXuats.ToList<HangSanXuat>();
+            ViewBag.HangSanXuats = new SelectList(hsx, "MaHangSanXuat", "TenHangSanXuat", selectedValue1);
+            return View();
+        }
+
+        [HttpPost]
+        public string DeleteImages(string id)
+        {
+            // Xóa ảnh con
+            int idhinh = Convert.ToInt32(id);
+            HinhAnhSanPham hsp = db.HinhAnhSanPhams.Where(s => s.MaHinh == idhinh).Single();
+            string path = Path.Combine(Server.MapPath("~/Content/images/products/"), hsp.HinhURL);
+            if ((System.IO.File.Exists(path)))
             {
-                // chưa có thì mặc định là hình 1
-                lstImages[0].Status = true;
-            }
-            // Save ảnh
-             foreach (var item in lstImages)
-            {
-                if (lstImages.Count > 0)
-                {
-                    // Hình chọn làm ảnh đại diện
-                    if(item.Status == true)
-                    {
-                        fileName = "p-"+ idmax.ToString() + "." + item.Extension ;
-                        sp.HinhURL = fileName;
-                        path = Path.Combine(Server.MapPath("~/Content/images/products"), fileName);
-                        // Hình đại diện
-                        Image image_root = item.Base64ToImage(item.Image);
-                        image_root.Save(path);                        
-                        db.SanPhams.Add(sp);
-                    }
-                    else
-                    {
-                        idmax2 = idmax2 + 1;
-                        // Upload vào table HinhAnhSanPham
-                        fileName = "slide-p-" + idmax.ToString() + "-" +idmax2.ToString() + "." + item.Extension;
-                        path = Path.Combine(Server.MapPath("~/Content/images/products/slide_products"), fileName);
-                        Image image = item.Base64ToImage(item.Image);
-                        image.Save(path);
-                        HinhAnhSanPham hssp = new HinhAnhSanPham();
-                        hssp.MaSanPham = sp.MaSanPham;
-                        hssp.HinhURL = fileName;
-                        db.HinhAnhSanPhams.Add(hssp);
-                    }
-                }                
-            }
-            if (db.SaveChanges() > 0)
-            {
-                return "Thêm thành công";
+                System.IO.File.Delete(path);
             }
             else
             {
-                return "Thêm thất bại";
+                return "error";
             }
+            db.HinhAnhSanPhams.Remove(hsp);
+            if (db.SaveChanges() > 0)
+            {
+                return "sucess";
+            }
+            else
+            {
+                return "error";
+            }
+
         }
+
+
     }
 }
